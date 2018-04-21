ee248d71ea51a8d2ba3c99b11bae016daaf5f36d
Josh Stafford <joshstaffhome@gmail.com>
2018-04-21 10:40:27 +0100

    Turn-based system
    Began work on turn-based system adding starting and stopping of players

---
+++ Assets/TurnController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TurnController : MonoBehaviour {
+
+    public FirstPersonController player1;
+    public FirstPersonController player2;
+    private int turnCounter = 0;
+    private float p_DefaultSpeed;
+    private float P_DefaultJumpSpeed;
+    private float p_DefaultXSens;
+    private float p_DefaultYSens;
+    private int noPlayers;
+    private float turnLength;
+    private bool turn;
+
+    private void Start()
+    {
+        p_DefaultSpeed = player1.m_WalkSpeed;
+        P_DefaultJumpSpeed = player1.m_JumpSpeed;
+        p_DefaultXSens = player1.m_MouseLook.XSensitivity;
+        p_DefaultYSens = player1.m_MouseLook.YSensitivity;
+        player1.m_WalkSpeed = 0;
+        noPlayers = 2;
+        player2.m_WalkSpeed = 0;
+        turnLength = 5;
+        turn = false;
+    }
+
+    private void Main()
+    {
+        Turn();
+    }
+
+    private void Turn(FirstPersonController currentPlayer)
+    {
+        float time_remaining = turnLength;
+
+        while (turn)
+        {
+
+            startPlayer(currentPlayer);
+
+            time_remaining -= Time.deltaTime;
+
+            if (time_remaining <= 0) {
+
+                turn = false;
+                stopPlayer(currentPlayer);
+                // Broadcast turn has ended
+
+            };
+
+        }
+        // Do all the turn stuff
+
+
+    }
+
+    public void startPlayer(FirstPersonController player) {
+
+        player.m_WalkSpeed = p_DefaultSpeed;
+        player.m_RunSpeed = p_DefaultSpeed;
+        player.m_JumpSpeed = P_DefaultJumpSpeed;
+        player.m_MouseLook.XSensitivity = p_DefaultXSens;
+        player.m_MouseLook.XSensitivity = p_DefaultYSens;
+
+    }
+
+    public void stopPlayer (FirstPersonController player){
+
+        player.m_WalkSpeed = 0f;
+        player.m_RunSpeed = 0f;
+        player.m_JumpSpeed = 0f;
+        player.m_MouseLook.XSensitivity = 0f;
+        player.m_MouseLook.XSensitivity = 0f;
+
+    }
+}
